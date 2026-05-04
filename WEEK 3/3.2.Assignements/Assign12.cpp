493d00b72f976d09995317905987ed91cd919bde
YanisLab1 <anis.bhm.asr@gmail.com>
2026-05-04 18:02:45 +0100

    fix error assigement 12

--- WEEK 3/3.2.Assignements/Assign12.cpp
+++ WEEK 3/3.2.Assignements/Assign12.cpp
@@ -9,7 +9,7 @@ int b = 10000;
 long double c = 5.560000505012;
 
 // Change ??? To Something Else To Get The Output
-     cout << ((int(a*c)) * 2)/(b/a) - ((b+a)/(b/a))<< "\n"; // 12
+     cout << ((int(a*c)) * ((b/a)/int(c)))/(b/a) - ((b+a)/(b/a))<< "\n"; // 12
      cout << (b/a) + (b/a) << "\n"; // 20
      cout << sizeof(c) * sizeof(a) << "\n";
      cout << a * (int(c)) << "\n";
