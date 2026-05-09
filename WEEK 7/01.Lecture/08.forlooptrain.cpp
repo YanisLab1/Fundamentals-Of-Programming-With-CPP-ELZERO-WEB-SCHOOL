8c564c5293270592526ccfc0583a4befea9f90e4
YanisLab1 <anis.bhm.asr@gmail.com>
2026-05-09 12:14:29 +0100

    update

--- WEEK 7/01.Lecture/08.forlooptrain.cpp
+++ WEEK 7/01.Lecture/08.forlooptrain.cpp
@@ -33,126 +33,67 @@ int main()
   }
   cout << result << endl;
 
+  cout << "======================\n";
+
   // Guess The Number
+  int winNumber = 7;
+  int guessTries = 3;
+  int num;
+
+  cout << "Please Guess The Winning Number Between 1 & 10\n";
+
+  cin >> num;
+  while ( num < 1 || num > 10)
+  {
+    cout << "Invalid input, try again." << endl;
+    cin >> num;
+  }
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  while (num != winNumber)
+  {
+    guessTries--;
+    if (guessTries == 0)
+      {
+        cout << "Sorry, You Failed. The Number Is: " << winNumber << "\n";
+        break;
+      }
+    cout << "Sorry, Wrong Guess\n";
+    cout << "Tries Remaining : " << guessTries << "\n";
+    cin >> num;
+    while ( num < 1 || num > 10)
+    {
+      cout << "Invalid input, try again." << endl;
+      cin >> num;
+    }
   
+  if (num == winNumber)
+  {
+    cout << "Great, " << num << " is The Correct Guess\n";
   
-     /*
-     
-     Count Positive & Even Numbers Only
+  cout << "Thanks for playing!" <<endl;
+  }  
+  } 
 
-     int result = 0;
-     int nums[] = {10, 20, -20, 13, 30, -30, 40};
+cout << "======================\n";
 
-     for (int i = 0; i < size(nums); i++)
-     {
-          if ( nums[i] > 0 && nums[i] % 2 == 0)
-          {
-               result += nums[i];
-          }
-     }
-     cout << "Result Is: " << result << "\n"; 
-     
-     */
+int vals[5];
+int inp;
 
-     /* Guess The Number Game
+cout << "Type 5 Numbers To Reverse\n";
 
-     int winNumber = 7;
-     int guessTries = 3;
-     int num;
+for (int i = 4; i > -1; i--)
+{
+  cin >> inp;
+  vals[i] = inp;
+}
 
-     cout << "Please Guess The Winning Number Between 1 & 10\n";
-     cin >> num;
 
-     while ( num < 1 || num > 10)
-     {
-          cout << "Invalid input, try again." << endl;
-          cin >> num;
-     }
+cout << "======================\n";
 
-     while (num != winNumber)
-     {
-          guessTries--;
-          if (guessTries == 0)
-          {
-               cout << "Sorry, You Failed. The Number Is: " << winNumber << "\n";
-               break;
-          }
-          cout << "Sorry, Wrong Guess\n";
-          cout << "Tries Remaining : " << guessTries << "\n";
-          cin >> num;
+for (int i = 0; i < size(vals); i++)
+{
+  cout << vals[i] << "\n";
+}
 
-          while ( num < 1 || num > 10)
-          {
-               cout << "Invalid input, try again." << endl;
-               cin >> num;
-          }
-     }
-
-     if (num == winNumber)
-     {
-          cout << "Great, Correct Guess\n";
-     }
-
-     cout << "Thanks for playing!";
-     
-     
-     //*/
-
-     //
-
-//     int vals[5];
-     
-     //int inp;
-
-     //
-     
-     //cout << "Type 5 Numbers To Reverse\n";
-
-     //
-     
-     //for (int i = 4; i > -1; i--)
-     
-     //{
-     
-     //     cin >> inp;
-
-//          vals[i] = inp;
-     
-     //}
-
-     //
-     
-     //cout << "======================\n";
-
-     //
-     
-     //for (int i = 0; i < size(vals); i++)
-     
-     //{
-     //     cout << vals[i] << "\n";
-     //}
-     
-     return 0;
+  return 0;
 }
